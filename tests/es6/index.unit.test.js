a75f68ff489c9768564d41996015c1fe0f4d4075
zearkiatos <caprilespe@outlook.com>
2021-08-16 22:20:02 -0400

    Let and const multilines, spread operator and destructuration and dockerization project

--- tests/es6/index.unit.test.js
+++ tests/es6/index.unit.test.js
@@ -1,6 +1,6 @@
 const { newFunction, newFunctionES6 } = require("../../src/es6");
 describe("Test suite with demo change of ES6", () => {
-  test("Test params pass old way", () => {
+  test("Should test params pass old way", () => {
     const expectedResult = "Pedro, 32, VE";
 
     const result = newFunction();
@@ -8,7 +8,7 @@ describe("Test suite with demo change of ES6", () => {
     expect(expectedResult).toBe(result);
   });
 
-  test("Test params pass with default params ES6 way", () => {
+  test("Should test params pass with default params ES6 way", () => {
     const expectedResult = "Pedro, 32, VE";
 
     const result = newFunctionES6();
@@ -34,5 +34,79 @@ describe("Test suite with demo change of ES6", () => {
     let epicPhrase = `${hello} ${world}`;
 
     expect(expectedResult).toBe(epicPhrase);
+  });
+
+  test("Should set multiline text", () => {
+    let expectedResult =
+      "Qui consequatur. Commodi. Ipsum vel duis yet minima \nother epic phrase than we need";
+    let lorem =
+      "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" +
+      "other epic phrase than we need";
+
+    expect(expectedResult).toBe(lorem);
+  });
+
+  test("Should set multiline text in ES6", () => {
+    let expectedResult = `Other epic phrase than we need
+    now is another phrase epic`;
+
+    let lorem = `Other epic phrase than we need
+    now is another phrase epic`;
+
+    expect(expectedResult).toBe(lorem);
+  });
+
+  test("Should get object attributes", () => {
+    let person = {
+      name: "Pedro",
+      age: 32,
+      country: "VE",
+    };
+
+    expect("Pedro").toBe(person.name);
+    expect(32).toBe(person.age);
+  });
+
+  test("Should get object attributes with ES6 destructure", () => {
+    let person = {
+      name: "Pedro",
+      age: 32,
+      country: "VE",
+    };
+
+    const { name, age } = person;
+
+    expect("Pedro").toBe(name);
+    expect(32).toBe(age);
+  });
+
+  test("Should concat arrays with propagation mode with ES6", () => {
+    const expectedResult = [
+      "David",
+      "Pedro",
+      "Julian",
+      "Ricardo",
+      "Valeria",
+      "Yessica",
+      "Camila",
+    ];
+    const team1 = ["Pedro", "Julian", "Ricardo"];
+
+    const team2 = ["Valeria", "Yessica", "Camila"];
+
+    const education = ["David", ...team1, ...team2];
+
+    expect(expectedResult).toEqual(education);
+  });
+
+  test("Should evalue let declaration and var declaration", () => {
+    globalLet = undefined;
+    const fn = function () {
+      var global = "Global Var";
+      let globalLet = "Global Let";
+    };
+
+    expect(global).toBeDefined();
+    expect(globalLet).toBe(undefined);
   });
 });
